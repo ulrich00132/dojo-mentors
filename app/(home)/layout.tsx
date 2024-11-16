52c08b0dfb08b930a5d1e53319db3abfad54d522
ulrich00132 <47371955+ulrich00132@users.noreply.github.com>
2024-11-16 16:58:54 +0100

    Update layout.tsx exorus tag 2

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -71,7 +71,12 @@ export default async function RootLayout({
       </Script>
 
       {/* Exorus Tracking Code */}
-      <script defer data-domain="dojomentors.com" src="/tracker/user-activity-tracker.js"></script>
+    <script
+      defer
+      data-domain="dojomentors.com"
+      src="http://localhost:3000/tracker/user-activity-tracker.js">
+    </script>
+
   
       </head>
       <body className={font.className}>
